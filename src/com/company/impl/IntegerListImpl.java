c7f09660c6c3fc2dbac28252d25778255a0eceb2
Dmitry <leon1861@yandex.ru>
2022-07-17 17:04:50 +0700

    Снова я ни чего не понял!

--- src/com/company/impl/IntegerListImpl.java
+++ src/com/company/impl/IntegerListImpl.java
@@ -4,57 +4,58 @@ import com.company.exception.ArrayIsFullException;
 import com.company.exception.ElementIsNullException;
 import com.company.exception.IndexMoreThanElementsException;
 import com.company.exception.RemoveNonExistentElementException;
+import com.company.interfaces.IntegerList;
 import com.company.interfaces.StringList;
 
 import java.util.Arrays;
 
-public class StringListImpl implements StringList {
+public class IntegerListImpl implements IntegerList {
 
-    private String[] stringList;
+    private Integer[] integerList;
     private int size;
 
-    public StringListImpl() {
-        stringList = new String[10];
+    public IntegerListImpl() {
+        integerList = new Integer[10];
     }
 
-    public StringListImpl(int initSize) {
+    public IntegerListImpl(int initSize) {
         if (initSize <= 0) {
             throw new IllegalArgumentException("Размер массива должен быть положительным");
         }
-        stringList = new String[initSize];
+        integerList = new Integer[initSize];
     }
 
 
     @Override
-    public String add(String item) {
+    public Integer add(Integer item) {
         sizeValid();
         itemValid(item);
-        stringList[size++] = item;
+        integerList[size++] = item;
         return item;
     }
 
     @Override
-    public String add(int index, String item) {
+    public Integer add(int index, Integer item) {
         sizeValid();
         itemValid(item);
         indexValid(index);
 
         for (int i = size; i > index; i--) {
-            stringList[i] = stringList[i - 1];
+            integerList[i] = integerList[i - 1];
         }
         size++;
-       return stringList[index] = item;
+        return integerList[index] = item;
     }
 
     @Override
-    public String set(int index, String item) {
+    public Integer set(int index, Integer item) {
         indexValid(index);
         itemValid(item);
-       return stringList[index] = item;
+        return integerList[index] = item;
     }
 
     @Override
-    public String remove(String item) {
+    public Integer remove(Integer item) {
         itemValid(item);
 
         int index = lastIndexOf(item);
@@ -63,7 +64,7 @@ public class StringListImpl implements StringList {
         }
 
         if (index != size) {
-            System.arraycopy(stringList, index + 1, stringList, index, size - index);
+            System.arraycopy(integerList, index + 1, integerList, index, size - index);
         }
 
         size--;
@@ -71,13 +72,13 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public String remove(int index) {
+    public Integer remove(int index) {
         indexValid(index);
 
-        String item = stringList[index];
+        Integer item = integerList[index];
 
         if (index != size) {
-            System.arraycopy(stringList, index + 1, stringList, index, size - index);
+            System.arraycopy(integerList, index + 1, integerList, index, size - index);
         }
 
         size--;
@@ -85,14 +86,32 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public boolean contains(String item) {
-        return indexOf(item) != -1;
+    public boolean contains(Integer item) {
+        itemValid(item);
+        sort();
+        int min = 0;
+        int max = size - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (item.equals(integerList[mid])) {
+                return true;
+            }
+
+            if (item < integerList[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
     }
 
     @Override
-    public int indexOf(String item) {
+    public int indexOf(Integer item) {
         for (int i = 0; i < size; i++) {
-            String s = stringList[i];
+            Integer s = integerList[i];
             if (s.equals(item)) {
                 return i;
             }
@@ -101,9 +120,9 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public int lastIndexOf(String item) {
+    public int lastIndexOf(Integer item) {
         for (int i = size - 1; i >= 0; i--) {
-            String s = stringList[i];
+            Integer s = integerList[i];
             if (s.equals(item)) {
                 return i;
             }
@@ -112,13 +131,13 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public String get(int index) {
+    public Integer get(int index) {
         indexValid(index);
-        return stringList[index];
+        return integerList[index];
     }
 
     @Override
-    public boolean equals(StringList otherList) {
+    public boolean equals(IntegerList otherList) {
         return Arrays.equals(this.toArray(), otherList.toArray());
     }
 
@@ -138,18 +157,31 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public String[] toArray() {
-        return Arrays.copyOf(stringList, size);
+    public Integer[] toArray() {
+        return Arrays.copyOf(integerList, size);
     }
 
-    private void itemValid(String item) {
+    private void sort() {
+        for (int i = 1; i < size; i++) {
+            int temp = integerList[i];
+            int j = i;
+            while (j > 0 && integerList[j - 1] >= temp) {
+                integerList[j] = integerList[j - 1];
+                j--;
+            }
+            integerList[j] = temp;
+        }
+    }
+
+
+    private void itemValid(Integer item) {
         if (item == null) {
             throw new ElementIsNullException();
         }
     }
 
     private void sizeValid() {
-        if (size == stringList.length) {
+        if (size == integerList.length) {
             throw new ArrayIsFullException();
         }
     }
